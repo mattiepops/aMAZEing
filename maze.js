7e1ca4831d16404d09c83edaded4846959bb27d9
Matt <Matt@Mateis-MacBook-Pro.local>
2020-11-25 16:54:47 +0100

    added readme and did the final touches for the maze

--- maze.js
+++ maze.js
@@ -1,7 +1,19 @@
+document.body.addEventListener("keydown", function(e){
+    if (e.code == "F12"){
+    alert("NOOO PLZ DON'T OPEN PANDOR'S BOX! All the monsters will come out :((")
+    }
+})
+
+level = 0;
 const body = document.querySelector("body");
 body.className = "body";
 
-const multiline = `***********.*
+let x, y;
+let emptyArr = [];
+
+
+
+let lvl1 = `***********.*
 *S.....**.*.T
 *****.....*.*
 *****.***.*.*
@@ -13,46 +25,96 @@ const multiline = `***********.*
 *.******...**
 *....********`;
 
+let lvl2 = `**********************
+*..S.................*
+********************.*
+*....................*
+*.********************
+*...................T*`;
 
-const lineArr = multiline.split("\n");
-console.log(lineArr);
+let lvl3 = `********
+****S***
+****.***
+****.***
+****.***
+*......*
+*.****.*
+*..***.*
+*..***.*
+**.*****
+*T.*****
+********`
 
-for (let i = 0; i <= lineArr.length - 1; i++) {
-    const lineDiv = document.createElement('div');
-    lineDiv.classList.add('lineDiv');
-    lineDiv.id = i
-    const line = lineArr[i];
-    for (let j = 0; j <= line.length - 1; j++) {
-        const characterDiv = document.createElement('div');
-        const character = line.split("")
-        characterDiv.innerHTML = character[j];
-        characterDiv.classList.add('tile');
-        lineDiv.appendChild(characterDiv);
-        if (characterDiv.innerHTML == "*") {
-            characterDiv.classList.add("wall");
-            characterDiv.innerHTML = "";
-        } else if (characterDiv.innerHTML == ".") {
-            characterDiv.classList.add('path');
-            characterDiv.innerHTML = "";
-        } else if (characterDiv.innerHTML == "S") {
-            characterDiv.classList.add('start');
-            characterDiv.innerHTML = "";
-        } else if (characterDiv.innerHTML == "T") {
-            characterDiv.classList.add('end');
-            characterDiv.innerHTML = "";
+let lvl4 = 
+`********.T.*
+**S..**..*.*
+****.****.**
+.**..****..*
+....*...*.**
+***...*...**
+****.*******`
+
+const multiline = [lvl1, lvl2, lvl3, lvl4];
+
+
+function leveling() {
+
+    const lineArr = multiline[level].split("\n");
+    console.log(lineArr);
+
+    for (let i = 0; i < lineArr.length; i++) {
+        const lineDiv = document.createElement('div');
+        emptyArr[i] = [];
+
+        lineDiv.classList.add('lineDiv');
+        lineDiv.id = i
+        const line = lineArr[i];
+        for (let j = 0; j <= line.length - 1; j++) {
+            const characterDiv = document.createElement('div');
+            emptyArr[i][j] = characterDiv;
+            const character = line.split("")
+            characterDiv.innerHTML = character[j];
+            characterDiv.classList.add('tile');
+            lineDiv.appendChild(characterDiv);
+            if (characterDiv.innerHTML == "*") {
+                characterDiv.classList.add("wall");
+                characterDiv.innerHTML = "";
+            } else if (characterDiv.innerHTML == ".") {
+                characterDiv.classList.add('path');
+                characterDiv.innerHTML = "";
+            } else if (characterDiv.innerHTML == "S") {
+                characterDiv.classList.add('start');
+                characterDiv.innerHTML = "";
+                const mario = document.createElement('div');
+                mario.className = "mario";
+                characterDiv.appendChild(mario);
+                x = j;
+                y = i;
+
+            } else if (characterDiv.innerHTML == "T") {
+                characterDiv.classList.add('end');
+                characterDiv.innerHTML = "";
+            }
+
         }
-
+        body.appendChild(lineDiv)
     }
-    body.appendChild(lineDiv)
-
+    console.log(emptyArr)
 }
 
-let x = 2;
-let y = 3;
 
-const mario = document.createElement('div');
-mario.className = "mario";
-document.querySelector("#\\31  > div.tile.start").appendChild(mario);
+
+// let x = 2;
+// let y = 3;
+
+// const mario = document.createElement('div');
+// mario.className = "mario";
+// document.querySelector("#\\31  > div.tile.start").appendChild(mario);
+
+
+
+
+// GOOMBAAAAAAAAA _____________
 
 const box = document.createElement('div');
 box.className = "box"
@@ -96,77 +158,128 @@ function mainLoop(timestamp) {
 
 requestAnimationFrame(mainLoop);
 
+// END GOOMBA _________________
+
+
 
 
 document.body.addEventListener('keydown', function (e) {
+
+    const mario = document.querySelector('.mario');
+
+    let destination;
+
     if (e.code == 'ArrowUp') {
-        if (document.querySelector("body > div:nth-child(" + (y - 1) + ")> div:nth-child(" + x + ")").classList.contains('wall')) {
+        y--;
+        destination = emptyArr[y][x];
+        if (destination.classList.contains('wall')) {
             console.log('hit a wall');
-        } else {
-            console.log('moved up');
-            y--;
-            document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+            y++;
+            destination = emptyArr[y][x];
         }
+        // if (document.querySelector("body > div:nth-child(" + (y - 1) + ")> div:nth-child(" + x + ")").classList.contains('wall')) {
+        //     console.log('hit a wall');
+        // } else {
+        //     console.log('moved up');
+        //     y--;
+        //     document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+        // }
     }
     if (e.code == 'ArrowRight') {
-        if (document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + (x + 1) + ")").classList.contains('wall')) {
+        x++;
+        destination = emptyArr[y][x];
+        if (destination.classList.contains('wall')) {
             console.log('hit a wall');
-        } else {
-            console.log('moved right');
-            x++;
-            console.log(x);
-            document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+            x--;
+            destination = emptyArr[y][x];
         }
+
+
+        // if (document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + (x + 1) + ")").classList.contains('wall')) {
+        //     console.log('hit a wall');
+        // } else {
+        //     console.log('moved right');
+        //     x++;
+        //     console.log(x);
+        //     document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+        // }
     }
     if (e.code == 'ArrowDown') {
-        if (document.querySelector("body > div:nth-child(" + (y + 1) + ")> div:nth-child(" + x + ")").classList.contains('wall')) {
+        y++;
+        destination = emptyArr[y][x];
+        if (destination.classList.contains('wall')) {
             console.log('hit a wall');
-        } else {
-            console.log('moved down');
-            y++;
-            document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+            y--;
+            destination = emptyArr[y][x];
         }
+        // if (document.querySelector("body > div:nth-child(" + (y + 1) + ")> div:nth-child(" + x + ")").classList.contains('wall')) {
+        //     console.log('hit a wall');
+        // } else {
+        //     console.log('moved down');
+        //     y++;
+        //     document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+        // }
     }
     if (e.code == 'ArrowLeft') {
-        if (document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + (x - 1) + ")").classList.contains('wall')) {
+        x--;
+        destination = emptyArr[y][x];
+        if (destination.classList.contains('wall')) {
             console.log('hit a wall');
-        } else {
-            console.log('moved left');
-            x--
-            document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+            x++;
+            destination = emptyArr[y][x];
         }
+        // if (document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + (x - 1) + ")").classList.contains('wall')) {
+        //     console.log('hit a wall');
+        // } else {
+        //     console.log('moved left');
+        //     x--
+        //     document.querySelector("body > div:nth-child(" + y + ")> div:nth-child(" + x + ")").appendChild(mario);
+        // }
+
+
     }
-    if (y == 3 && x == 13) {
+
+    destination.appendChild(mario);
+
+    if (destination.classList.contains('end')) {
         alert('YOU DID IT WAHOOOOO')
+        x = 0;
+        y = 0;
+        destination = emptyArr[y][x];
+        body.innerHTML = "";
+        level++;
+        leveling()
     }
 
 })
+leveling();
 
 
-// let seconds = 0;
-// let minutes = 0;
-// const section = document.createElement('section')
-// const time = document.createElement('div')
-// const p = document.createElement('p');
-// p.className = 'timer';
-// time.appendChild(p)
-// section.appendChild(time)
-// body.appendChild(section)
+// TIMER _______________________ //
 
 
+let seconds = 0;
+let minutes = 0;
+const section = document.createElement('section')
+const time = document.createElement('div')
+const p = document.createElement('p');
+p.className = 'timer';
+time.appendChild(p)
+section.appendChild(time)
+body.appendChild(section)
 
-// function timer() {
-//     const para = document.querySelector('p');
-//     if (seconds>=0){
-//         para.textContent = seconds + ' s';
-//         if (seconds>60){
-//             para.textContent = minutes + ' min ' + (seconds - (minutes *60)) + ' s'
-//         }
-//     }
-//     if (seconds % 60 == 0 && seconds !== 0) {
-//         para.textContent = (minutes +1 ) + ' min';
-//         minutes++
-//     }
-//     seconds++
-// }
-// const interval = setInterval(timer, 1000)
+function timer() {
+    // const para = document.querySelector('p');
+    if (seconds>=0){
+        p.textContent = seconds + ' s';
+        if (seconds>60){
+            para.textContent = minutes + ' min ' + (seconds - (minutes *60)) + ' s'
+        }
+    }
+    if (seconds % 60 == 0 && seconds !== 0) {
+        para.textContent = (minutes +1 ) + ' min';
+        minutes++
+    }
+    seconds++
+}
+const interval = setInterval(timer, 1000)
---
+++ readme.md
@@ -0,0 +1,35 @@
+# This is aMAZEing!
+________
+
+Wecome ***Kelian*** and/or ***Emily*** to my masterpiece! :bowtie:
+
+Please find the **maze project link** [here] ().
+
+### Reason for this project:
+________
+
+We were asked to put our newly discovered JS skills to test, by creating a basic 2D maze game and coding an automatic level advancement functionality into the game. 
+
+We were asked to code all elements and functions directly in JS.
+
+
+### What did I learn from this experience:
+
+I learned how to convert a .txt file into an actual maze, by arranging a class to each element of the .txt file and then assigning style rules to each class. Furthermore, I have learned how to generate multiple levels of the maze into my code, each new level appearing after completing the previous one. I am very thankful to indian guys on Youtube, and also to my colleagues at BeCode for helping me out when I needed help. 
+
+## Problems encountered:
+
+I was stuck on the movements of my crackhead Mario in the maze, and I tried a few different ways to code the function of the addEventListener, and eventually after 4 completely different attempts, I have managed to make the italian man get off his ass and move!
+
+Later on, it was really not obvious for me how to even begin thinking about adding multiple levels to my maze, and how I would code all of them so that the next level starts when you complete the current level. I had a crash course with Maxime and Axel, from Jean, who was super patient and explained us step by step how to do it. 
+
+I added a goomba as an enemy, but unfortunateily I did not know how to make it so that if the player collides with the goomba, the level is restarted.
+
+Finally, I was not able to make it so that the timer and goomba appear in every level. 
+
+### Conclusion
+_____
+
+In the end, I powered through and managed to create an amazeing maze!
+
+Thank you for reading this readme and giving me feedback on the project :punch: 
